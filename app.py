060532c78a2c08546c62799e2bd9d5f66cdb4c4b
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-05 13:57:22 -0300

    adding pyautogui

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 # Passo a passo do projeto
 # Passo 1: entrar no sistema da empresa 
     #  https://dlp.hashtagtreinamentos.com/python/intensivao/login
-    
-    
+import pyautogui
+
+# pyautogui.click -> clicar em algum lugar da tela
+# pyautogui.write -> escrever um texto
+# pyautogui.press -> pressionar 1 tecla do teclado
+
+# abrir o navegador
+pyautogui.press("home")
+pyautogui.press("search")
+pyautogui.write("chrome")
+pyautogui.click()
+
 # Passo 2: fazer login
 # Passo 3: importar a base de dados para fazer o cadastro
 # Passo 4 : cadastrar um produto
