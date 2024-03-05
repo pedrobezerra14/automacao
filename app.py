7c518eb28992447dd33f993d7a24f468b35bfb8e
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-05 18:48:33 -0300

    adding time to project

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-# Passo a passo do projeto
-# Passo 1: entrar no sistema da empresa 
-    #  https://dlp.hashtagtreinamentos.com/python/intensivao/login
 import pyautogui
+import time
 
-# pyautogui.click -> clicar em algum lugar da tela
-# pyautogui.write -> escrever um texto
-# pyautogui.press -> pressionar 1 tecla do teclado
-
-# abrir o navegador
 pyautogui.press("home")
+time.sleep(1)
 pyautogui.press("search")
-pyautogui.write("chrome")
-pyautogui.click()
-
-# Passo 2: fazer login
-# Passo 3: importar a base de dados para fazer o cadastro
-# Passo 4 : cadastrar um produto
-# Passo 5 : repetir o processo até acabar a base de dados
-
+time.sleep(1)
+pyautogui.write("brave")
+time.sleep(1)
+pyautogui.press("enter")
+time.sleep(1)
