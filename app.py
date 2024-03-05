9157e0c204c0a91a8a4c9286aadb0f3cebc3f208
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-05 13:28:28 -0300

    adding app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# Passo a passo do projeto
+# Passo 1: entrar no sistema da empresa 
+    #  https://dlp.hashtagtreinamentos.com/python/intensivao/login
+    
+    
+# Passo 2: fazer login
+# Passo 3: importar a base de dados para fazer o cadastro
+# Passo 4 : cadastrar um produto
+# Passo 5 : repetir o processo até acabar a base de dados
+
